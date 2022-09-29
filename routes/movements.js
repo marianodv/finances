2a88d9d7c2e4f18eec503a195521e303c6257d98
Mariano <marianodv@outlook.com>
2022-09-29 16:03:30 -0300

    add router.post in routes/movements

--- routes/movements.js
+++ routes/movements.js
@@ -6,4 +6,8 @@ const movementsController = require('../controllers/movementsController')
 /* GET all movements */
 router.get('/', movementsController.getAll);
 
+/* POST one movement */
+/* require: in body json whit concept(string), amount(decimal) and isEgress(boolean)*/
+router.post('/', movementsController.create);
+
 module.exports = router;
