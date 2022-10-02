2fff878de4181650526ae9a83c5dafa51b5a3b6c
Mariano <marianodv@outlook.com>
2022-10-01 23:21:45 -0300

    create categoriesModel

---
+++ models/categoriesModel.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../config/sequelizedb')
+
+const categoriesSchema = sequelize.define('categories',{
+    _id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        unique:true,
+        primaryKey:true
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:{
+                args:[3,25],
+                msg:'Debe escribir entre 3 y 25 caracteres en nombre de categoria.'
+            },
+            notNull:{
+                args:[true],
+                msg:'El campo nombre no puede ser nulo.'
+            }
+        },
+        set(value){
+            this.setDataValue('name',new String(value).toLowerCase())
+        }
+    }
+})
+
+
+module.exports = movementsSchema
