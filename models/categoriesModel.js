a9270c1c5b4af2dfe0efef68b0d9b221e10e87f9
Mariano <marianodv@outlook.com>
2022-10-02 00:02:58 -0300

    change log of sincronize in models

--- models/categoriesModel.js
+++ models/categoriesModel.js
@@ -37,7 +37,7 @@ const sincronize = async ()=>{
  *  sync(): Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, no lo sobrescribirá.
  *  sync({force: true}) Método: Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, lo sobrescribirá.
  */
-    console.log("sincronized!")
+    console.log("categories sincronized!")
 }
 
 sincronize();
--- models/movementsModel.js
+++ models/movementsModel.js
@@ -60,7 +60,7 @@ const sincronize = async ()=>{
  *  sync(): Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, no lo sobrescribirá.
  *  sync({force: true}) Método: Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, lo sobrescribirá.
  */
-    console.log("sincronized!")
+    console.log("movements sincronized!")
 }
 
 sincronize();
