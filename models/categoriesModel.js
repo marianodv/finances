016837c348a6fd44777a41c1891f134ca795381d
Mariano <marianodv@outlook.com>
2022-10-02 00:05:53 -0300

    delete sync() in categories model. Only one sync()

--- models/categoriesModel.js
+++ models/categoriesModel.js
@@ -31,15 +31,4 @@ const categoriesSchema = sequelize.define('categories',{
 
 
 
-const sincronize = async ()=>{
-    await sequelize.sync()
-/**
- *  sync(): Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, no lo sobrescribirá.
- *  sync({force: true}) Método: Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, lo sobrescribirá.
- */
-    console.log("categories sincronized!")
-}
-
-sincronize();
-
 module.exports = categoriesSchema
