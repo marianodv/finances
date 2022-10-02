5a16c96d77309395ff93c565c21120928b103c64
Mariano <marianodv@outlook.com>
2022-10-02 00:01:09 -0300

    add route to categories in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const movementsRouter = require('./routes/movements')
+const categoriesRouter = require('./routes/categories')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movements',movementsRouter)
+app.use('/categories',categoriesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
