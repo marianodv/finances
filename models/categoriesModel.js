8429e0558f770728d79a9af3539137469c99d4bf
Mariano <marianodv@outlook.com>
2022-10-02 00:02:12 -0300

    add sequelize.sync() in categoriesModel

--- models/categoriesModel.js
+++ models/categoriesModel.js
@@ -30,4 +30,16 @@ const categoriesSchema = sequelize.define('categories',{
 })
 
 
+
+const sincronize = async ()=>{
+    await sequelize.sync()
+/**
+ *  sync(): Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, no lo sobrescribirá.
+ *  sync({force: true}) Método: Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, lo sobrescribirá.
+ */
+    console.log("sincronized!")
+}
+
+sincronize();
+
 module.exports = categoriesSchema
