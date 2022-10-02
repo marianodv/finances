5072262b704348e828101ffdf985a31848288799
Mariano <marianodv@outlook.com>
2022-10-01 23:54:57 -0300

    create categories.js in routes

---
+++ routes/categories.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const categoriesController = require('../controllers/categoriesController')
+
+/* GET all categories */
+router.get('/', categoriesController.getAll);
+
+/* POST one category */
+/* require: in body json name(string)*/
+router.post('/', categoriesController.create);
+
+/* PUT one category */
+/* require: in params: id */
+/* require: in body json name(string)*/
+router.put('/category/:id', categoriesController.modifyById);
+
+/* GET one category */
+//router.get('/category/:id', categoriesController.getById);
+
+/* DELETE one category */
+router.delete('/category/:id', categoriesController.deleteById);
+
+module.exports = router;
