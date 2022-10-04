c6af439a03444590cb62360eb04cf2fea50b47b1
Mariano <marianodv@outlook.com>
2022-10-04 15:46:36 -0300

    create page CreateMovement

---
+++ front-react/src/Pages/CreateMovement.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from "react";
+
+function CreateMovement(){
+
+
+    useEffect(
+        ()=>{
+            
+        },
+        []
+    )
+
+    return(
+        <>
+            loading....
+        </>
+    )
+
+    
+}
+
+export default CreateMovement
+
+
