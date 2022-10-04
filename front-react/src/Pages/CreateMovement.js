1da82a3e5331aa291cb31b05a61b2acf644f3577
Mariano <marianodv@outlook.com>
2022-10-04 18:14:30 -0300

    use Input Component in CreateMovement

--- front-react/src/Pages/CreateMovement.js
+++ front-react/src/Pages/CreateMovement.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useForm} from "react-hook-form"
+import Input from "../Components/Input"
 
 function CreateMovement(){
 
@@ -12,26 +13,18 @@ function CreateMovement(){
     return(
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>Fecha</label>
-                    <input type="text" {...register("name",{required:true})}/>
-                    {errors.name && <span>El campo nombre es obligatorio.</span>}
-                </div>
-                <div>
-                    <label>Concepto</label>
-                    <input type="text" {...register("concept",{required:true})}/>
-                    {errors.concept && <span>El campo concepto es obligatorio.</span>}
-                </div>
-                <div>
-                    <label>Monto</label>
-                    <input type="number" {...register("amount",{required:true,min:0})}/>
-                    {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
-                    {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
-                </div>
-                <div>
-                    <label>Es Egreso: </label>
-                    <input type="checkbox" {...register("isEgress",{value:true})} />
-                </div>
+                <Input label="Fecha" register={{...register("name",{required:true})}}/>
+                {errors.name && <span>El campo nombre es obligatorio.</span>}
+            
+                <Input label="Concepto" register={{...register("concept",{required:true})}}/>
+                {errors.concept && <span>El campo concepto es obligatorio.</span>}
+
+                <Input label="Monto" type="number" register={{...register("amount",{required:true,min:0})}}/>
+                {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
+                {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
+
+                <Input label="Es Egreso:" type="checkbox" register={{...register("isEgress",{value:true})}}/>
+                
                 <button type="submit">AGREGAR</button>
             </form>
         </div>
