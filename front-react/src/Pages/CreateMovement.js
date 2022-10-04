786b72d61e4c3863ee52227a81d99a3e231da943
Mariano <marianodv@outlook.com>
2022-10-04 18:02:56 -0300

    create form

--- front-react/src/Pages/CreateMovement.js
+++ front-react/src/Pages/CreateMovement.js
@@ -14,18 +14,23 @@ function CreateMovement(){
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label>Fecha</label>
-                    <input type="text" {...register("name")}/>
+                    <input type="text" {...register("name",{required:true})}/>
+                    {errors.name && <span>El campo nombre es obligatorio.</span>}
                 </div>
                 <div>
                     <label>Concepto</label>
-                    <input type="text" {...register("concept")}/>
+                    <input type="text" {...register("concept",{required:true})}/>
+                    {errors.concept && <span>El campo concepto es obligatorio.</span>}
                 </div>
                 <div>
                     <label>Monto</label>
-                    <input type="number" {...register("amount")}/>
+                    <input type="number" {...register("amount",{required:true,min:0})}/>
+                    {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
+                    {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
                 </div>
                 <div>
-                    <input type="checkbox" {...register("isEgress")} value="Es Egreso?" />
+                    <label>Es Egreso: </label>
+                    <input type="checkbox" {...register("isEgress",{value:true})} />
                 </div>
                 <button type="submit">AGREGAR</button>
             </form>
