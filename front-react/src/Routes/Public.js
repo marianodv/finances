53f79dee6922f87c7faa864449eee1941807d7e3
Mariano <marianodv@outlook.com>
2022-10-04 15:47:58 -0300

    add CreateMovement to Routes

--- front-react/src/Routes/Public.js
+++ front-react/src/Routes/Public.js
@@ -1,6 +1,7 @@
 import Home from "../Pages/Home"
 import Movements from "../Pages/Movements"
 import Detail from "../Pages/Detail"
+import CreateMovement from "../Pages/CreateMovement"
 import NotFound from "../Pages/NotFound"
 import {Routes, Route} from "react-router-dom"
 
@@ -9,6 +10,7 @@ function Public(){
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movements" element={<Movements />} />
+            <Route path="/movements/create" element={<CreateMovement />} />
             <Route path="/movements/detail/:id" element={<Detail />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
