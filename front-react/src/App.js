1296b4f40e1dcf7242eedd630f56a95cc4d2e6e7
Mariano <marianodv@outlook.com>
2022-10-04 15:43:49 -0300

    agregate Menu component

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Public from './Routes/Public';
+import Menu from './Components/Menu'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Menu />
         <Public />
       </Router>
     </div>
