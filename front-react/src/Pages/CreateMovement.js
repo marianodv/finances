2bd49765b8acb382a2821d5687c38855ef023e5d
Mariano <marianodv@outlook.com>
2022-10-04 16:22:08 -0300

    install react-hook-forms and use

--- front-react/src/Pages/CreateMovement.js
+++ front-react/src/Pages/CreateMovement.js
@@ -1,19 +1,35 @@
-import React, {useEffect} from "react";
+import React from "react";
+import {useForm} from "react-hook-form"
 
 function CreateMovement(){
 
+    const { register, handleSubmit, formState:{errors}} = useForm()
 
-    useEffect(
-        ()=>{
-            
-        },
-        []
-    )
+     const onSubmit = (data) => {
+        console.log("FORM ", data)
+     }
 
     return(
-        <>
-            loading....
-        </>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>Fecha</label>
+                    <input type="text" {...register("name")}/>
+                </div>
+                <div>
+                    <label>Concepto</label>
+                    <input type="text" {...register("concept")}/>
+                </div>
+                <div>
+                    <label>Monto</label>
+                    <input type="number" {...register("amount")}/>
+                </div>
+                <div>
+                    <input type="checkbox" {...register("isEgress")} value="Es Egreso?" />
+                </div>
+                <button type="submit">AGREGAR</button>
+            </form>
+        </div>
     )
 
     
