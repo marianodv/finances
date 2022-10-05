2beff2ff3bcae9284432e5a440cfadbc46e8e1ed
Mariano <marianodv@outlook.com>
2022-10-05 15:39:16 -0300

    probe categories

--- front-react/src/Components/Categories.js
+++ front-react/src/Components/Categories.js
@@ -1,12 +1,25 @@
 import React,{useEffect,useState} from "react";
+import {getAll} from "../Services/categoriesService"
 
 function Categories(){
 
     const [loading,setLoading] = useState(true)
+    const [categories, setCategories] = useState[]
 
     useEffect(
         ()=>{
-            
+            const request = async () => {
+                try{
+                    const cat = await getAll()
+                    console.log("cat: ", cat.data)
+                    setCategories(cat.data)
+                    setLoading(false)
+                }catch (error){
+                    console.log("Error: ", error)
+                }
+            }
+
+            request()
         },
         []
     )
