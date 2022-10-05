d0bb1724c6d315d5fe3e5752662318ae3bfc8c81
Mariano <marianodv@outlook.com>
2022-10-05 16:08:02 -0300

    change to see category name

--- front-react/src/Components/Movement.js
+++ front-react/src/Components/Movement.js
@@ -3,19 +3,19 @@ import {Link} from "react-router-dom"
 
 function Movement(props){
 
-    const {_id,concept,amount,date,categoryId,isEgress} = props.data
+    const {_id,concept,amount,date,category,isEgress} = props.data
     const children = props.children
 
     return(
         <>
             { isEgress &&
                 <div>
-                    <p><Link to={"/movements/detail/" + _id}>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({categoryId || ''}) | $-{amount || ''}</Link></p>
+                    <p><Link to={"/movements/detail/" + _id}>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({category?.name || "Without Category"}) | $-{amount || ''}</Link></p>
                 </div>
             }
             { !isEgress &&
                 <div>
-                    <p><Link to={"/movements/detail/" + _id}>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({categoryId || ''}) | ${amount || ''}</Link></p>
+                    <p><Link to={"/movements/detail/" + _id}>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({category?.name || "Without Category"}) | ${amount || ''}</Link></p>
                 </div>
             }
         </>
