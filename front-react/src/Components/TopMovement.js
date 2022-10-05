964f7ac0c07e9e5a502b7561146cf3758626d9d2
Mariano <marianodv@outlook.com>
2022-10-05 16:24:30 -0300

    change topMovement to see category name

--- front-react/src/Components/TopMovement.js
+++ front-react/src/Components/TopMovement.js
@@ -3,19 +3,19 @@ import React from "react";
 
 function TopMovement(props){
 
-    const {concept,amount,date,categoryId,isEgress} = props.data
+    const {concept,amount,date,category,isEgress} = props.data
     const children = props.children
 
     return(
         <>
             { isEgress &&
                 <div>
-                    <h4>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({categoryId || ''}) | $-{amount || ''}</h4>
+                    <h4>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({category?.name || "Without Category"}) | $-{amount || ''}</h4>
                 </div>
             }
             { !isEgress &&
                 <div>
-                    <h3>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({categoryId || ''}) | ${amount || ''}</h3>
+                    <h3>{children})_{new Date(date).toLocaleDateString() || ''} | {concept || ''} ({category?.name || "Without Category"}) | ${amount || ''}</h3>
                 </div>
             }
         </>
