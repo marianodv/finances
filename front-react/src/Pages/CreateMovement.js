ce53bab538760dd0b6a1e38901bf4be08dff41ff
Mariano <marianodv@outlook.com>
2022-10-06 00:32:07 -0300

    simplified return

--- front-react/src/Pages/CreateMovement.js
+++ front-react/src/Pages/CreateMovement.js
@@ -54,7 +54,7 @@ function CreateMovement(props){
                 setOperation(true)
             }
         },
-        [setValue]
+        [setValue,op]
     )
 
     const handleChange = ()=>{
@@ -62,57 +62,30 @@ function CreateMovement(props){
         console.log(operation)
     }
 
-    return(
-        <>
-            { operation &&
-                <div style={styles.egress}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Input label="Fecha" type="date" register={{...register("date",{required:true})}}/>
-                        {errors.name && <span>El campo nombre es obligatorio.</span>}
-                    
-                        <Input label="Concepto" register={{...register("concept",{required:true})}}/>
-                        {errors.concept && <span>El campo concepto es obligatorio.</span>}
+    
+    return( 
+        <div style={(operation && styles.egress) || (!operation && styles.ingress)}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input label="Fecha" type="date" register={{...register("date",{required:true})}}/>
+                {errors.name && <span>El campo nombre es obligatorio.</span>}
+            
+                <Input label="Concepto" register={{...register("concept",{required:true})}}/>
+                {errors.concept && <span>El campo concepto es obligatorio.</span>}
 
-                        <Input label="Monto" type="number" register={{...register("amount",{required:true,min:0})}}/>
-                        {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
-                        {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
+                <Input label="Monto" type="number" register={{...register("amount",{required:true,min:0})}}/>
+                {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
+                {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
 
-                        <Categories label="Categoria: " register={{...register("categoryId")}}/>
+                <Categories label="Categoria: " register={{...register("categoryId")}}/>
 
-                        <div>
-                            <label>Es Egreso: </label>
-                            <input type="checkbox" onChange={handleChange} checked={operation} register={{...register("isEgress")}}/>
-                        </div>
-                        
-                        <button type="submit">AGREGAR</button>
-                    </form>
+                <div>
+                    <label>Es Egreso: </label>
+                    <input type="checkbox" onChange={handleChange} checked={operation} register={{...register("isEgress")}}/>
                 </div>
-            }
-            { !operation &&
-                <div style={styles.ingress}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Input label="Fecha" type="date" register={{...register("date",{required:true})}}/>
-                        {errors.name && <span>El campo nombre es obligatorio.</span>}
-                    
-                        <Input label="Concepto" register={{...register("concept",{required:true})}}/>
-                        {errors.concept && <span>El campo concepto es obligatorio.</span>}
-
-                        <Input label="Monto" type="number" register={{...register("amount",{required:true,min:0})}}/>
-                        {errors.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
-                        {errors.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
-
-                        <Categories label="Categoria: " register={{...register("categoryId")}}/>
-
-                        <div>
-                            <label>Es Egreso: </label>
-                            <input type="checkbox" onChange={handleChange} checked={operation} register={{...register("isEgress")}}/>
-                        </div>
-                        
-                        <button type="submit">AGREGAR</button>
-                    </form>
-                </div>
-            }
-        </>
+                
+                <button type="submit">AGREGAR</button>
+            </form>
+        </div>
     )
 
     
