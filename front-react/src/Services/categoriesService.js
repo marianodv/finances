036156cce5b77fcb5ed9603daf212696140bdaa6
Mariano <marianodv@outlook.com>
2022-10-05 21:36:25 -0300

    newCategory in services

--- front-react/src/Services/categoriesService.js
+++ front-react/src/Services/categoriesService.js
@@ -3,3 +3,7 @@ import axios from "../Config/axios"
 export async function getAll(){
     return await axios.get('/categories/')
 }
+
+export async function newCategory(data){
+    return await axios.post('/categories/',data)
+}
