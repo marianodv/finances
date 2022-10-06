01478b174db8c90c778eb271b798e2e8129889b9
Mariano <marianodv@outlook.com>
2022-10-05 21:20:57 -0300

    add categories to routes

--- front-react/src/Routes/Public.js
+++ front-react/src/Routes/Public.js
@@ -4,6 +4,7 @@ import CreateMovement from "../Pages/CreateMovement"
 import EditMovement from "../Pages/EditMovement"
 import NotFound from "../Pages/NotFound"
 import {Routes, Route} from "react-router-dom"
+import Categories from "../Pages/Categories"
 
 function Public(){
     return(
@@ -13,6 +14,7 @@ function Public(){
             <Route path="/movements/create/:op" element={<CreateMovement />} />
             <Route path="/movements/create/" element={<CreateMovement />} />
             <Route path="/movements/edit/:id" element={<EditMovement />} />
+            <Route path="/categories/" element={<Categories />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
