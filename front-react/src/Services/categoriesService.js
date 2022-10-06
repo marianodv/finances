1bc84866a5172582466be760180d53143b84f329
Mariano <marianodv@outlook.com>
2022-10-05 21:46:30 -0300

    add delete and update in service

--- front-react/src/Services/categoriesService.js
+++ front-react/src/Services/categoriesService.js
@@ -7,3 +7,11 @@ export async function getAll(){
 export async function newCategory(data){
     return await axios.post('/categories/',data)
 }
+
+export async function deleteById(id){
+    return await axios.delete('/categories/category/' + id)
+}
+
+export async function updateById(id, data){
+    return await axios.put('/categories/category/' + id, data)
+}
