07020cb73c689455454d07dae0fe8f0c823a5e20
Mariano <marianodv@outlook.com>
2022-10-07 00:48:58 -0300

    add filters in Movements Page

--- front-react/src/Pages/Movements.js
+++ front-react/src/Pages/Movements.js
@@ -1,29 +1,73 @@
 import React, {useEffect,useState} from "react";
 import Movement from "../Components/Movement";
-import {getAll,deleteById} from "../Services/movementsServices"
+import {getAll,deleteById, getIncomes, getExpenses, getByCategory} from "../Services/movementsServices"
+import CategoriesList from "../Components/CategoriesList"
+import {useForm} from "react-hook-form"
 
 function Movements(){
 
     const [loading,setLoading] = useState(true)
     const [movements,setMovements] = useState([])
 
+    const { register,setValue, getValues} = useForm()
+
     useEffect(
         ()=>{
-            const request = async()=>{ 
-                try{          
-                    const response = await getAll()
-                    console.log("LST: ",response?.data)
-                    setMovements(response?.data)
-
-                    setLoading(false)
-                }catch (error){
-                    console.log("Error: ", error)
-                }
-            }
-            request()
+            listAll()
         },
-        [loading]
+        []
     )
+
+    const listAll = async () =>{
+        try{          
+            setValue("category",0)
+            const response = await getAll()
+            console.log("LST: ",response?.data)
+            setMovements(response?.data)
+
+            setLoading(false)
+        }catch (error){
+            console.log("Error: ", error)
+        }
+    }
+
+    const listForCategory = async () =>{
+        try{          
+            const response = await getByCategory(getValues("category"))
+            console.log("LST: ",response?.data)
+            setMovements(response?.data)
+
+            setLoading(false)
+        }catch (error){
+            console.log("Error: ", error)
+        }
+    }
+
+    const listIngress = async () =>{
+        try{    
+            setValue("category",0)      
+            const response = await getIncomes()
+            console.log("LST: ",response?.data)
+            setMovements(response?.data)
+
+            setLoading(false)
+        }catch (error){
+            console.log("Error: ", error)
+        }
+    }
+
+    const listEgress = async () =>{
+        try{      
+            setValue("category",0)    
+            const response = await getExpenses()
+            console.log("LST: ",response?.data)
+            setMovements(response?.data)
+
+            setLoading(false)
+        }catch (error){
+            console.log("Error: ", error)
+        }
+    }
 
     const handleDelete = async (id)=>{
         console.log("EEEEE", id)
@@ -38,6 +82,7 @@ function Movements(){
         }
     }
 
+  
     if(loading){
         return(
             <>
@@ -46,10 +91,34 @@ function Movements(){
         )
     }else{
         return(
-            <div>
-                {movements?.rows.map((movement,ind) => <Movement key={ind} data={movement} onDelete={()=>{handleDelete(movement._id)}}>{ind+1}</Movement>)}  
-                <p>{movements?.pageMin} to page {movements?.page} to {movements?.pageMax} | TOTAL: {movements?.rowsCount} | listed: {movements?.rowsPerPage}</p>
-            </div>
+            <>
+                <div>
+                    <CategoriesList label="Categorias: " register={{...register("category")}}/>
+                    <button onClick={()=>{
+                        setLoading(true)
+                        listForCategory()
+                    }}>Por Categoria</button>
+
+                    <button onClick={()=>{
+                        setLoading(true)
+                        listAll()
+                    }}>TODO</button>
+
+                    <button onClick={()=>{
+                        setLoading(true)
+                        listIngress()
+                    }}>Solo Ingresos</button>
+
+                    <button onClick={()=>{
+                        setLoading(true)
+                        listEgress()
+                    }}>Solo Egresos</button>
+                </div>
+                <div>
+                    {movements?.rows.map((movement,ind) => <Movement key={ind} data={movement} onDelete={()=>{handleDelete(movement._id)}}>{ind+1}</Movement>)}  
+                    <p>{movements?.pageMin} to page {movements?.page} to {movements?.pageMax} | TOTAL: {movements?.rowsCount} | listed: {movements?.rowsPerPage}</p>
+                </div>
+            </>
         )
     }
     
--- front-react/src/Services/movementsServices.js
+++ front-react/src/Services/movementsServices.js
@@ -8,6 +8,18 @@ export async function getAll(){
     return await axios.get('/movements/')
 }
 
+export async function getIncomes(){
+    return await axios.get('/movements/incomes/')
+}
+
+export async function getExpenses(){
+    return await axios.get('/movements/expenses/')
+}
+
+export async function getByCategory(id){
+    return await axios.get('/movements/category/' + id)
+}
+
 export async function getById(id){
     return await axios.get('/movements/movement/' + id)
 }
