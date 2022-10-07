474a91c8b2e1a099c166ab84df42657056ed6f27
Mariano <marianodv@outlook.com>
2022-10-07 01:36:02 -0300

    create component FormMovement

---
+++ front-react/src/Components/FormMovemet.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Input from "../Components/Input"
+
+function FormMovement(props){
+
+    const {children, submit, dateRegister, conceptRegister, amountRegister, categoryRegister, isEgressRegister, error} = props
+
+    return(
+        <form onSubmit={submit}>
+            <Input label="Fecha" type="date" register={dateRegister}/>
+            {error?.name && <span>El campo nombre es obligatorio.</span>}
+        
+            <Input label="Concepto" register={conceptRegister}/>
+            {error?.concept && <span>El campo concepto es obligatorio.</span>}
+
+            <Input label="Monto" type="number" register={amountRegister}/>
+            {error?.amount?.type === 'required' && <span>El campo monto es obligatorio.</span>}
+            {error?.amount?.type === 'min' && <span>El monto no puede ser negativo.</span>}
+
+            <CategoriesList label="Categoria: " register={categoryRegister}/>
+
+            
+            {isEgressRegister &&
+                <div>
+                    <label>Es Egreso: </label>
+                    <input type="checkbox" onChange={handleChange} checked={operation} register={isEgressRegister}/>
+                </div>
+            }
+            
+            <div>
+                <button type="submit">GUARDAR</button>
+                {children}
+            </div>
+        </form>
+    )
+}
+
+export default FormMovement
